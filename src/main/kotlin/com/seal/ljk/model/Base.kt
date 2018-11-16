9bbe467e7c8d568d39999b19e2497e913936d15e
jonz <jon-zoro@hotmail.com>
2018-11-16 15:39:02 +0800

    修改时间类型

--- src/main/kotlin/com/seal/ljk/model/Base.kt
+++ src/main/kotlin/com/seal/ljk/model/Base.kt
@@ -1,11 +1,10 @@
 package com.seal.ljk.model
 
-import java.text.SimpleDateFormat
 import java.util.*
 
 open class Base(
-    var createDate: String = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date()),
+    var createDate: Date = Date(),
     var createUser: String = "",
-    var updateDate: String = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date()),
+    var updateDate: Date = Date(),
     var updateUser: String = ""
 )
