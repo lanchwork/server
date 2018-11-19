eeafb8bfd61ae2b15048931413c47e051f085e74
jonz <jon-zoro@hotmail.com>
2018-11-19 11:27:25 +0800

    模糊查询修改

--- src/main/kotlin/com/seal/ljk/Provider/InvestDetailProvider.kt
+++ src/main/kotlin/com/seal/ljk/Provider/InvestDetailProvider.kt
@@ -10,19 +10,19 @@ class InvestDetailProvider {
     fun queryWalletInvestDetail(qInvestDetail: QInvestDetail): String{
         val element = " investor_wallet_addr,  invest_amt, invest_period, day_rate, invest_date, expire_date, " +
                 "pre_rcv_amt, partner_id, partner_wallet_addr, invest_id "
-        var sql: SQL = SQL().SELECT(element).FROM(INVEST_DETAIL)
+        val sql: SQL = SQL().SELECT(element).FROM(INVEST_DETAIL)
 
         val investorWalletAddr = qInvestDetail.investorWalletAddr
         if(investorWalletAddr.isNotEmpty()){
-            sql.WHERE(" investor_wallet_addr LIKE #{investorWalletAddr}")
+            sql.WHERE(" investor_wallet_addr LIKE concat('%',#{investorWalletAddr},'%') ")
         }
         val partnerName = qInvestDetail.partnerName
         if(partnerName.isNotEmpty()){
-            sql.WHERE(" partner_name LIKE #{partnerName}")
+            sql.WHERE(" partner_name LIKE concat('%',#{partnerName},'%') ")
         }
         val investId = qInvestDetail.investId
         if(investId.isNotEmpty()){
-            sql.WHERE(" invest_id LIKE #{investId}")
+            sql.WHERE(" invest_id LIKE concat('%',#{investId},'%') ")
         }
         val investPeriodFrom = qInvestDetail.investPeriodFrom
         if(investPeriodFrom.isNotEmpty()){
@@ -39,23 +39,23 @@ class InvestDetailProvider {
     fun queryPaybackInvestDetail(qInvestDetail: QInvestDetail): String{
         val element = " investor_wallet_addr,  invest_amt, invest_period, day_rate, invest_date, expire_date, " +
                 "pre_rcv_amt, actual_rcv_date, actual_rcv_amt, partner_id, partner_wallet_addr, status, invest_id "
-        var sql: SQL = SQL().SELECT(element).FROM(INVEST_DETAIL)
+        val sql: SQL = SQL().SELECT(element).FROM(INVEST_DETAIL)
 
         val investorWalletAddr = qInvestDetail.investorWalletAddr
         if(investorWalletAddr.isNotEmpty()){
-            sql.WHERE(" investor_wallet_addr LIKE #{investorWalletAddr}")
+            sql.WHERE(" investor_wallet_addr LIKE concat('%',#{investorWalletAddr},'%') ")
         }
         val partnerName = qInvestDetail.partnerName
         if(partnerName.isNotEmpty()){
-            sql.WHERE(" partner_name LIKE #{partnerName}")
+            sql.WHERE(" partner_name LIKE concat('%',#{partnerName},'%') ")
         }
         val investId = qInvestDetail.investId
         if(investId.isNotEmpty()){
-            sql.WHERE(" invest_id LIKE #{investId}")
+            sql.WHERE(" invest_id LIKE concat('%',#{investId},'%') ")
         }
         val status = qInvestDetail.status
         if(status.isNotEmpty()){
-            sql.WHERE(" status LIKE #{status}")
+            sql.WHERE(" status LIKE concat('%',#{status},'%') ")
         }
         val currentPage = (qInvestDetail.currentPage - 1) * qInvestDetail.pageSize
         return sql.toString() + " limit " + currentPage.toString() + ", " + qInvestDetail.pageSize.toString()
