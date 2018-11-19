b3ffe874b4057b7880d380ef475b64be66de1f9e
guoantu <459953592@qq.com>
2018-11-19 17:19:49 +0800

    我的投资-投资未还款列表

--- src/main/kotlin/com/seal/ljk/dao/InvestDetailDao.kt
+++ src/main/kotlin/com/seal/ljk/dao/InvestDetailDao.kt
@@ -14,6 +14,13 @@ interface InvestDetailDao {
             "from invest_detail where investor_wallet_addr=#{investorWalletAddr} and status=2 limit #{currentPage}, #{pageSize}")
     fun getRepaymentList(@Param("investorWalletAddr") investorWalletAddr : String,@Param("currentPage") currentPage: Int, @Param("pageSize") pageSize: Int): List<InvestDetail>
 
+    /**
+     * 已投资未回款列表
+     */
+    @Select("select invest_detail_id,invest_id,chain_trans_no,investor_wallet_addr,invest_amt,invest_period,day_rate,invest_date,expire_date,pre_rcv_principal,pre_income,pre_rcv_amt,actual_rcv_date,actual_rcv_principal,actual_rcv_interest,actual_rcv_amt,partner_id,partner_wallet_addr,overdue_day,status,create_date,create_user,update_date,update_user,remark " +
+            "from invest_detail where investor_wallet_addr=#{investorWalletAddr} and status in (1,3) limit #{currentPage}, #{pageSize}")
+    fun getNonReturnList(@Param("investorWalletAddr") investorWalletAddr : String,@Param("currentPage") currentPage: Int, @Param("pageSize") pageSize: Int): List<InvestDetail>
+
     @Select("select sum(actual_rcv_principal) from invest_detail where investor_wallet_addr=#{investorWalletAddr} and status=2")
     fun getActualRcvPrincipalSum(@Param("investorWalletAddr") investorWalletAddr : String): BigDecimal
 
