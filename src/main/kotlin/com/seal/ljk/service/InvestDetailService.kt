935b28474a94fb49cce864f5f04da9aadf3364ef
jonz <jon-zoro@hotmail.com>
2018-11-27 11:09:34 +0800

    删除旧的投资表相关文件

--- src/main/kotlin/com/seal/ljk/service/InvestDetailService.kt
+++ src/main/kotlin/com/seal/ljk/service/InvestDetailService.kt
@@ -1,6 +1,5 @@
 package com.seal.ljk.service
 
-import com.seal.ljk.dao.InvestDao
 import com.seal.ljk.dao.InvestDetailDao
 import com.seal.ljk.model.InvestDetail
 import com.seal.ljk.model.InvestDetailList
@@ -13,22 +12,6 @@ class InvestDetailService {
 
     @Autowired
     lateinit var investDetailDao: InvestDetailDao
-    @Autowired
-    lateinit var investDao: InvestDao
-
-
-    /*投资已还款列表查询*/
-    fun getRepaymentList(investorWalletAddr: String,currentPage: Int, pageSize: Int):InvestDetailList {
-        var resulList= InvestDetailList()
-        //投资总金额和已收总利息
-        val invest = investDao.getPartnerById(investorWalletAddr)
-        resulList.totalInvestAmt = invest.totalInvestAmt
-        resulList.earnedAmt = invest.earnedAmt
-        //已还款列表查询
-        resulList.list = investDetailDao.getRepaymentList(investorWalletAddr,currentPage, pageSize)
-
-        return resulList
-    }
 
     /***
      * 授权投资明细查询
