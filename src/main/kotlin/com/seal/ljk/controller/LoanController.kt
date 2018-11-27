3914bb131a9969b592399550f98f6ad56319eaf1
kangxj <kangxj@s-labs.cn>
2018-11-27 10:59:41 +0800

    loan由还款表改成放款表

--- src/main/kotlin/com/seal/ljk/controller/LoanController.kt
+++ src/main/kotlin/com/seal/ljk/controller/LoanController.kt
@@ -2,9 +2,11 @@ package com.seal.ljk.controller
 
 import com.seal.ljk.common.ResVal
 import com.seal.ljk.model.Loan
+import com.seal.ljk.query.QLoan
 import com.seal.ljk.service.LoanService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
+import java.lang.Exception
 
 @RestController
 @RequestMapping("/loan")
@@ -15,32 +17,19 @@ class LoanController {
     lateinit var loanService: LoanService
 
     /**
-     * 借款利息支付统计查询
-     */
-    @GetMapping("/getById")
-    fun getPartnerById(@RequestParam loanerWalletAddr: String): ResVal {
-        val result: Loan
+     *  根据投资编号查询放款投资明细
+     * */
+    @RequestMapping("/getLoanList")
+    fun getLoanListByInvestNo(@RequestBody qLoan: QLoan): ResVal {
+        val resultList: List<Loan>
         try {
-            result=loanService.getPartnerById(loanerWalletAddr)
+            resultList = loanService.getLoanListByInvestNo(qLoan)
         } catch (e: Exception) {
             e.printStackTrace()
             return ResVal(1, "Data Access Error!")
         }
-        return ResVal(0, result)
+        return ResVal(0, resultList)
     }
 
-    /***
-     * 我要还款
-     */
-    @RequestMapping("/want")
-    fun saveWantRepay(@RequestBody data: Map<String, Any>): ResVal {
-        try {
-            loanService.saveWantRepay(data)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            return ResVal(1, "Data Access Error!")
-        }
-        return ResVal(0, "SUCCESS")
-    }
 
 }
--- src/main/kotlin/com/seal/ljk/dao/LoanDao.kt
+++ src/main/kotlin/com/seal/ljk/dao/LoanDao.kt
@@ -1,27 +1,16 @@
 package com.seal.ljk.dao
 
 import com.seal.ljk.model.Loan
-import org.apache.ibatis.annotations.Insert
-import org.apache.ibatis.annotations.Param
-import org.apache.ibatis.annotations.Select
-import org.apache.ibatis.annotations.Update
+import com.seal.ljk.provider.LoanProvider
+import com.seal.ljk.query.QLoan
+import org.apache.ibatis.annotations.SelectProvider
 import org.springframework.stereotype.Repository
 
 @Repository
 interface LoanDao {
 
-    @Select("select loan_id,loaner_wallet_addr,total_repay_amt,total_loan_amt,pay_interest,unpay_interest,total_pay_amt,create_date,create_user,update_date,update_user,remark " +
-            "from loan where loaner_wallet_addr = #{loanerWalletAddr}")
-    fun getPartnerById(@Param("loanerWalletAddr") loanerWalletAddr : String): Loan
+    @SelectProvider(type = LoanProvider::class, method = "queryLoan")
+    fun getLoanListByInvestNo(qLoan: QLoan): List<Loan>
 
-    @Select("select loan_id, loaner_wallet_addr, total_repay_amt, total_loan_amt, pay_interest, unpay_interest, total_pay_amt, create_date, create_user, update_date, update_user, remark from loan where loan_id = #{loanId}")
-    fun getLoanById(@Param("loanId") loanId: String): Loan
 
-    @Insert("insert into loan(loan_id, loaner_wallet_addr, total_repay_amt, total_loan_amt, pay_interest, unpay_interest, total_pay_amt, create_date, create_user, update_date, update_user, remark) " +
-            "values(#{loan.loanId}, #{loan.loanerWalletAddr}, #{loan.totalRepayAmt}, #{loan.totalLoanAmt}, #{loan.payInterest}, #{loan.unpayInterest}, #{loan.totalPayAmt}, #{loan.createDate}, #{loan.createUser}, #{loan.updateDate}, #{loan.updateUser}, #{loan.remark})")
-    fun createLoan(@Param("loan") loan: Loan)
-
-    @Update("update loan set loan_id=#{loan.loanId}, loaner_wallet_addr=#{loan.loanerWalletAddr}, total_repay_amt=#{loan.totalRepayAmt}, total_loan_amt=#{loan.totalLoanAmt}, pay_interest=#{loan.payInterest}, unpay_interest=#{loan.unpayInterest}, total_pay_amt=#{loan.totalPayAmt}, create_date=#{loan.createDate}, create_user=#{loan.createUser}, update_date=#{loan.updateDate}, update_user=#{loan.updateUser}, remark=#{loan.remark}" +
-            "where loan_id = #{loan.loanId}")
-    fun updateLoanById(@Param("loan") loan: Loan)
 }
