408d8dbc7fe062b34bf83ae8212a09d8c1412389
tingyx <98668177@qq.com>
2018-11-29 19:33:21 +0800

    新增对账统计

--- src/main/kotlin/com/seal/ljk/dao/ReconciliationDao.kt
+++ src/main/kotlin/com/seal/ljk/dao/ReconciliationDao.kt
@@ -22,4 +22,46 @@ interface ReconciliationDao {
 
     @SelectProvider(type = ReconciliationProvider::class, method = "getReconciliationByCondition")
     fun getReconciliationByCondition(qReconciliation: QReconciliation): List<Reconciliation>
+
+    /**
+     * 统计该合作方收到投资人的投资金额总和，即合作方的收款金额
+     */
+    @Select("select sum(invest_amt) as rcv_amt  from invest_detail where partner_id=#{partnerId}")
+    fun sumInvestAmtByPartnerId(partnerId: String): Map<String, Any>
+
+    /**
+     * 统计该合作方在前一天的放款金额总和
+     */
+    @Select("select sum(actual_loan_amt) as loan_amt " +
+            "from loan " +
+            "where invest_no in (select invest_no from invest_detail where partner_id =#{partnerId}) " +
+            "and loan_time>=date_format(date(now())-1,'%Y-%m-%d %H:%i:%s') " +
+            "and loan_time<date_format(date(now()),'%Y-%m-%d %H:%i:%s')")
+    fun sumLoanAmtByPartnerId(partnerId: String): Map<String, Any>
+
+    /**
+     * 统计所有投资人对该合作方发起的结算金额、投资人分润、seal分润
+     */
+    @Select("select sum(apply_settle_amt) as settle_amt,sum(investor_profit) as invest_total_amt,sum(seal_profit) as seal_total_profit " +
+            "from settlement where partner_id=#{partnerId}")
+    fun sumSettlementByPartnerId(partnerId: String): Map<String, Any>
+
+    /**
+     * 统计该合作方未结算本金之和
+     */
+    @Select("select sum(unsettled_principal) as unsettled_principal from invest_detail where partner_id=#{partnerId}")
+    fun sumUnsettledPrincipalByPartnerId(partnerId: String): Map<String, Any>
+
+    /**
+     * 统计自上次结算后还款的总分润
+     */
+    @Select("select sum(l.investor_profit) as investor_profit " +
+            "from loan as l " +
+            "join " +
+            "(select invest_no,max(settle_time) as settle_time " +
+            " from settlement " +
+            " where partner_id = '123' " +
+            " group by invest_no) as g " +
+            "on l.invest_no=g.invest_no and l.repay_time>settle_time")
+    fun sumProfitByPartnerId(partnerId: String):Map<String, Any>
 }
--- src/main/kotlin/com/seal/ljk/service/ReconciliationService.kt
+++ src/main/kotlin/com/seal/ljk/service/ReconciliationService.kt
@@ -5,11 +5,16 @@ import com.seal.ljk.model.Reconciliation
 import com.seal.ljk.query.QReconciliation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
+import java.util.*
 
 @Service
 class ReconciliationService {
     @Autowired
     lateinit var reconciliationDao: ReconciliationDao
+
+    @Autowired
+    lateinit var partnerService: PartnerService
 
     fun getAllReconciliation(): List<Reconciliation> {
         return reconciliationDao.getAllReconciliation()
@@ -25,5 +30,57 @@ class ReconciliationService {
 
     fun getReconciliationByCondition(qReconciliation: QReconciliation): List<Reconciliation> {
         return reconciliationDao.getReconciliationByCondition(qReconciliation)
+    }
+
+    /**
+     * @Description: 对账数据的统计方法
+     * @Author: tingyx
+     * @Time: 2018-11-28 11:12
+     */
+    fun calculateReconciliation(){
+        val allPartners = partnerService.getAllPartner()
+        println("test")
+        //每天对每一个合作方进行一次统计
+        allPartners.forEach {
+            //对账主键ID
+            val reconciliationId = UUID.randomUUID().toString().substring(0, 20)
+            println(reconciliationId)
+            //合作方ID
+            val partnerId = it.partnerId
+            //统计日期
+            val statisticsDate = Date()
+
+            val rcvMap = reconciliationDao.sumInvestAmtByPartnerId(partnerId)
+            //收款金额
+            val rcvAmt = BigDecimal(rcvMap["rcvAmt"].toString())
+
+            val loanMap = reconciliationDao.sumLoanAmtByPartnerId(partnerId)
+            //放款金额
+            val loanAmt = BigDecimal(loanMap["loanAmt"].toString())
+
+            val settledMap = reconciliationDao.sumSettlementByPartnerId(partnerId)
+            //结算金额
+            val settleAmt = BigDecimal(settledMap["settleAmt"].toString())
+            //投资人总分润
+            val investorTotalProfit = BigDecimal(settledMap["investorTotalProfit"].toString())
+            //seal总分润
+            val sealTotalProfit = BigDecimal(settledMap["sealTotalProfit"].toString())
+
+            //计算待结算余额=上一次结算时还在款中的金额(已存储在投资明细表的未结算本金字段)+上一次结算后还款的利润
+            val unsettledMap = reconciliationDao.sumUnsettledPrincipalByPartnerId(partnerId)
+            val profitMap = reconciliationDao.sumProfitByPartnerId(partnerId)
+            //未结算本金
+            val unsettledPrincipal = BigDecimal(unsettledMap["unsettledPrincipal"].toString())
+            //自上次结算后还款的利润
+            val investorProfit = BigDecimal(profitMap["investorProfit"].toString())
+            //待结算余额
+            val balance = unsettledPrincipal + investorProfit
+
+            val reconciliation = Reconciliation(reconciliationId, partnerId, "", statisticsDate, rcvAmt, loanAmt, settleAmt,
+                    balance, investorTotalProfit, sealTotalProfit, "","")
+
+            println(reconciliation.toString())
+
+        }
     }
 }
