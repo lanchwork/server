1ac66cbe651a41462fb4c394afec56913fdeb267
liyq <liyq@s-labs.cn>
2018-12-03 17:37:20 +0800

    修改

---
+++ src/main/kotlin/com/seal/ljk/model/DataDetailLocalChain.kt
@@ -0,0 +1,8 @@
+package com.seal.ljk.model
+
+data class DataDetailLocalChain(
+        var updateTime: String = "",
+        var secondHash: String = "",
+        var linkLocalHash: String = "",
+        var localHash: String = ""
+)
