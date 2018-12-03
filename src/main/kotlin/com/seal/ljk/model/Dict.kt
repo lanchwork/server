4f9b070ba48e33b43b948aa457952e33ab36a39f
liyq <liyq@s-labs.cn>
2018-12-03 10:15:44 +0800

    修改

---
+++ src/main/kotlin/com/seal/ljk/model/Dict.kt
@@ -0,0 +1,11 @@
+package com.seal.ljk.model
+
+data class Dict(
+
+        var dictId: String = "",
+        var type: String = "",
+        var name: String = "",
+        var showVal: String = "",
+        var value: String = "",
+        var remark: String = ""
+)
