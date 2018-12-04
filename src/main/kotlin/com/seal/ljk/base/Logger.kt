1484cddc43ca9c5ed398394176942dc8ea6f8a96
Edward Chan <jin4074@gmail.com>
2018-12-04 13:31:36 +0800

    集成日志

--- src/main/kotlin/com/seal/ljk/base/Logger.kt
+++ src/main/kotlin/com/seal/ljk/base/Logger.kt
@@ -1,11 +1,12 @@
 package com.seal.ljk.base
 
-import org.slf4j.Logger
+import com.seal.ljk.MyApplication
 import org.slf4j.LoggerFactory
 
 /**
  * Created by cjh on 2018/12/4.
  */
-val <T : Any> T.logger: Logger
-    get() = LoggerFactory.getLogger(this.javaClass)
+fun <T> loggerFor(clazz: Class<T>) = LoggerFactory.getLogger(clazz)
+
+val logger = LoggerFactory.getLogger(MyApplication::class.java)
     
