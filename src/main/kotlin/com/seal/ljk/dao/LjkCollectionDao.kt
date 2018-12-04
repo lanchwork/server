2d5f323977cb9faee88b7e54cbfbb010dee12155
kangxj <kangxj@s-labs.cn>
2018-12-04 14:53:25 +0800

    根据逾期编号查看催收信息接口

--- src/main/kotlin/com/seal/ljk/dao/LjkCollectionDao.kt
+++ src/main/kotlin/com/seal/ljk/dao/LjkCollectionDao.kt
@@ -3,6 +3,9 @@ package com.seal.ljk.dao
 import com.seal.ljk.model.LjkCollection
 import com.seal.ljk.provider.LjkCollectionProvider
 import com.seal.ljk.query.QLjkCollection
+import com.seal.ljk.query.QLjkOverdue
+import org.apache.ibatis.annotations.Param
+import org.apache.ibatis.annotations.Select
 import org.apache.ibatis.annotations.SelectProvider
 import org.springframework.stereotype.Repository
 
@@ -11,5 +14,8 @@ interface LjkCollectionDao {
 
     @SelectProvider(type = LjkCollectionProvider::class, method = "queryLjkCollectionByConditions")
     fun queryLjkCollectionByConditions(qLjkCollection: QLjkCollection): List<LjkCollection>
+
+    @Select("select * from ljk_collection where channel_mark=#{channelMark} and channel_overdue_id=#{channelOverdueId}")
+    fun getLjkCollectionByConditions(@Param("channelMark")channelMark:String, @Param("channelOverdueId")channelOverdueId: String): List<LjkCollection>
 
 }
--- src/main/kotlin/com/seal/ljk/service/LjkOverdueService.kt
+++ src/main/kotlin/com/seal/ljk/service/LjkOverdueService.kt
@@ -1,6 +1,8 @@
 package com.seal.ljk.service
 
+import com.seal.ljk.dao.LjkCollectionDao
 import com.seal.ljk.dao.LjkOverdueDao
+import com.seal.ljk.model.LjkCollection
 import com.seal.ljk.model.LjkOverdue
 import com.seal.ljk.query.QLjkOverdue
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,8 +14,15 @@ class LjkOverdueService {
     @Autowired
     lateinit var ljkOverdueDao: LjkOverdueDao
 
+    @Autowired
+    lateinit var ljkCollectionDao: LjkCollectionDao
+
     fun queryLjkOverdueByConditions(qLjkOverdue: QLjkOverdue): List<LjkOverdue> {
         return ljkOverdueDao.queryLjkOverdueByConditions(qLjkOverdue)
+    }
+
+    fun getLjkCollectionByConditions(qLjkOverdue: QLjkOverdue): List<LjkCollection> {
+        return ljkCollectionDao.getLjkCollectionByConditions(qLjkOverdue.channelMark,qLjkOverdue.channelOverdueId)
     }
 
 }
