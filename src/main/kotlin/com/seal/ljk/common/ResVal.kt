9ebfd47908ee4c2248fb55e7eb07701243b5f395
liyq <liyq@s-labs.cn>
2018-12-26 09:14:44 +0800

    修改

--- src/main/kotlin/com/seal/ljk/common/ResVal.kt
+++ src/main/kotlin/com/seal/ljk/common/ResVal.kt
@@ -4,3 +4,9 @@ data class ResVal(
         val code: Int, // 0-success, 1-error
         val data: Any?
 )
+
+data class ResValMsg(
+        val code: Int, // 0-success, 1-error
+        val data: Any?,
+        val msg: String?
+)
