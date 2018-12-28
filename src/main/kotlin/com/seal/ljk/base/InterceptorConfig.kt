806df9124f003a88465d2b087f2ed42a54b64d49
Edward Chan <jin4074@gmail.com>
2018-12-28 15:46:55 +0800

    文件上传修改

--- src/main/kotlin/com/seal/ljk/base/InterceptorConfig.kt
+++ src/main/kotlin/com/seal/ljk/base/InterceptorConfig.kt
@@ -45,19 +45,6 @@ class InterceptorConfig : WebMvcConfigurer {
         return AuthenticationInterceptor()
     }
 
-    @Bean
-    fun multipartConfigElement(): MultipartConfigElement {
-        val factory = MultipartConfigFactory()
-        // 设置文件大小限制 ,超出设置页面会抛出异常信息，
-        // 这样在文件上传的地方就需要进行异常信息的处理了;
-        factory.setMaxFileSize("256KB") // KB,MB
-        /// 设置总上传数据总大小
-        factory.setMaxRequestSize("512KB");
-        // Sets the directory location where files will be stored.
-        // factory.setLocation("路径地址")
-        return factory.createMultipartConfig()
-    }
-
 }
 
 
