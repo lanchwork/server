772c1881992ea38d275a3aa3606b10214f6aadd8
kangxj <kangxj@s-labs.cn>
2018-12-29 11:18:30 +0800

    代持

---
+++ src/main/kotlin/com/seal/ljk/service/ITzAgencyHoldService.kt
@@ -0,0 +1,21 @@
+package com.seal.ljk.service
+
+import com.seal.ljk.model.TzAgencyHold
+import com.github.pagehelper.Page
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author kangxj
+ * @since 2018-12-29
+ */
+interface ITzAgencyHoldService{
+    fun getTzAgencyHold(id: String): TzAgencyHold?
+    fun getAllTzAgencyHold(tzAgencyHold: TzAgencyHold): List<TzAgencyHold>
+    fun getAllTzAgencyHoldByPage(tzAgencyHold: TzAgencyHold): Page<TzAgencyHold>
+    fun insertTzAgencyHold(tzAgencyHold: TzAgencyHold)
+    fun updateTzAgencyHold(tzAgencyHold: TzAgencyHold)
+    fun deleteTzAgencyHold(id: String)
+}
